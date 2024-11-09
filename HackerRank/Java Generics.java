6ba31906322cf35ab0bafa35081b7d719a8ce350
akshithaputta4 <137429777+akshithaputta4@users.noreply.github.com>
2024-11-09 12:20:35 +0530

    Create Java Generics.java

---
+++ HackerRank/Java Generics.java
@@ -0,0 +1,13 @@
+class Printer
+{
+   //Write your code here
+   public <T>void printArray(T[] arr)
+   {
+       int i;
+       for(i=0;i<arr.length;i++)
+       {
+           System.out.println(arr[i]);
+       }
+   }
+ 
+}
