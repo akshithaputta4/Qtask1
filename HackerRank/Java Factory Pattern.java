f30605a4969cf540aad93a559bafa47ec7322974
akshithaputta4 <137429777+akshithaputta4@users.noreply.github.com>
2024-11-09 12:41:04 +0530

    Create Java Factory Pattern.java

---
+++ HackerRank/Java Factory Pattern.java
@@ -0,0 +1,9 @@
+            if(order.equals("pizza"))
+            {
+                Pizza p=new Pizza();
+                return p;
+            }
+            else{
+                Cake c=new Cake();
+                return c;
+            }
