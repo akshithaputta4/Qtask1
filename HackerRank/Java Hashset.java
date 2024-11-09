90292c38a0e90c8e71fa42b6a26a8e91220d5dce
akshithaputta4 <137429777+akshithaputta4@users.noreply.github.com>
2024-11-09 12:19:16 +0530

    Create Java Hashset.java

---
+++ HackerRank/Java Hashset.java
@@ -0,0 +1,8 @@
+         int i;
+         Set<String> un=new HashSet<>();
+         for(i=0;i<t;i++)
+         {
+             String f=pair_left[i]+","+pair_right[i];
+             un.add(f);
+             System.out.println(un.size());
+         }
